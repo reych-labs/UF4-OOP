0657d7157db5262baa9e9607846b1a970e5340a4
Raquel <ralaman@institutmvm.cat>
2021-02-17 23:37:09 +0100

    Classes abstractes

---
+++ src/mvm/daw/exemples/Animal.java
@@ -0,0 +1,12 @@
+package mvm.daw.exemples;
+
+/**
+ * Classe abstracta
+ */
+abstract class Animal {
+    public abstract void animalSound();
+
+    public void sleep() {
+        System.out.println("Zzz");
+    }
+}
---
+++ src/mvm/daw/exemples/AnimalTest.java
@@ -0,0 +1,9 @@
+package mvm.daw.exemples;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
