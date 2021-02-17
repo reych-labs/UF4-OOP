f855a355e61afcd21a09dee62f3a3bcdeba6115c
Raquel <ralaman@institutmvm.cat>
2021-02-18 00:26:46 +0100

    Classes abstractes i polimorfisme

--- src/mvm/daw/exemples/AnimalTest.java
+++ src/mvm/daw/exemples/AnimalTest.java
@@ -3,7 +3,8 @@ package mvm.daw.exemples;
 public class AnimalTest {
     public static void main(String[] args) {
         Pig myPig = new Pig();
+        Dog myDog = new Dog();
         myPig.animalSound();
-        myPig.sleep();
+        myDog.animalSound();
     }
 }
