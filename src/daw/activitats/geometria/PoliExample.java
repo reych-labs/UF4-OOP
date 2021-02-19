6cce339a96b25c1a96dd810f36fc85b2cc79f4bb
raquel <ralaman@institutmvm.cat>
2021-02-19 17:38:24 +0100

    Polimorfism

---
+++ src/daw/activitats/geometria/PoliExample.java
@@ -0,0 +1,14 @@
+package daw.activitats.geometria;
+
+public class PoliExample {
+    public void print(String s){
+        System.out.println("El primer mètode amb Strings:"+ s);
+    }
+    public void print (int i){
+        System.out.println("El segon mètode amb enteres: "+ i);
+    }
+    public void print (String s, int i){
+        System.out.println("El tercer mètode amb els dos: - "+ s + "--" + i);
+    }
+}
+
---
+++ src/daw/activitats/geometria/PoliTest.java
@@ -0,0 +1,10 @@
+package daw.activitats.geometria;
+
+public class PoliTest {
+    public static void main(String[] args) {
+        PoliExample obj = new PoliExample();
+        obj.print(10);
+        obj.print("DAW");
+        obj.print("Hola", 100);
+    }
+}
